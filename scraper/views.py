80b751941f0035d5cc1838afc04d06239f142e3b
BahaaDesoky <desob1547@wrdsb.ca>
2020-06-04 14:48:28 -0400

    More css changes and updates (links)

--- scraper/views.py
+++ scraper/views.py
@@ -13,40 +13,44 @@ def home(request):
         item_price = []
         item_link = []
         item_shipping = []
+        item_condition = []
 
         nkw = request.POST['nkw']
-        url = 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313.TR8.TRC0.A0.H0.Xpokemon.TRS2&_nkw=' + nkw + '&_sacat=0'
+        url = 'https://www.ebay.com/sch/' + nkw
 
-        if 'https://' not in url:
-            messages.error(request, "Error")
-        else:
-            page = requests.get(url)
-            soup = BeautifulSoup(page.content, 'html.parser')
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
 
-            listings_title = soup.find_all('h3', class_='s-item__title')
-            listings_link = soup.find_all('a', class_='s-item__link')
-            listings_price = soup.find_all('span', class_='s-item__price')
-            listings_shipping = soup.find_all('span', class_='s-item__shipping s-item__logisticsCost')
+        listings_title = soup.find_all('h3', class_='s-item__title')
+        listings_link = soup.find_all('a', class_='s-item__link')
+        listings_price = soup.find_all('span', class_='s-item__price')
+        listings_shipping = soup.find_all('span', class_='s-item__shipping s-item__logisticsCost')
+        listings_condition = soup.find_all('span', class_='SECONDARY_INFO')
 
-            for listing in listings_title:
-                text_only = listing.text
-                no_newListing = text_only.replace('New Listing', '').replace('Ã—', '×')
-                item_title.append(no_newListing)
 
-            for listing in listings_price:
-                text_only = listing.text
-                no_dash = text_only.replace(' to ', '-')
-                item_price.append(no_dash)
+        for listing in listings_title:
+            text_only = listing.text
+            no_newListing = text_only.replace('New Listing', '').replace('Ã—', '×')
+            item_title.append(no_newListing)
 
-            for listing in listings_link:
-                item_link.append(listing['href'])
+        for listing in listings_price:
+            text_only = listing.text
+            no_dash = text_only.replace(' to ', '-')
+            item_price.append(no_dash)
 
-            for listing in listings_shipping:
-                text_only = listing.text
-                no_plus = text_only.replace('+', '').replace('Shipping', '').replace('shipping', '')
-                item_shipping.append(no_plus)
+        for listing in listings_link:
+            item_link.append(listing['href'])
 
-        master_list = zip(item_title, item_price, item_shipping, item_link)
+        for listing in listings_shipping:
+            text_only = listing.text
+            no_plus = text_only.replace('+', '').replace('Shipping', '').replace('shipping', '')
+            item_shipping.append(no_plus)
+
+        for listing in listings_condition:
+            text_only = listing.text
+            item_condition.append(text_only)
+
+        master_list = zip(item_title, item_price, item_shipping, item_condition, item_link)
         super_list = [list(a) for a in master_list]
 
         context = {
